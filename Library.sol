741bba69e4b5c93a437c9bf761891b483186079a
Heet Dedakiya <heetdedakiya@Heets-MacBook-Air.local>
2023-04-04 14:22:15 +0530

    initial

--- Library.sol
+++ Library.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "./interfaces/IZuniswapV2Factory.sol";
-import "./interfaces/IZuniswapV2Pair.sol";
-import {ZuniswapV2Pair} from "./ZuniswapV2Pair.sol";
+import "./interfaces/IFacory.sol";
+import "./interfaces/IPair.sol";
+import {DExPair} from "./DExPair.sol";
 
-library ZuniswapV2Library {
+library Library {
     error InsufficientAmount();
     error InsufficientLiquidity();
     error InvalidPath();
@@ -14,34 +14,34 @@ library ZuniswapV2Library {
         address factoryAddress,
         address tokenA,
         address tokenB
-    ) public returns (uint256 reserveA, uint256 reserveB){
+    ) public returns (uint256 reserveA, uint256 reserveB) {
         (address token0, address token1) = sortTokens(tokenA, tokenB);
-        (uint256 reserve0, uint256 reserve1) = IZuniswapV2Pair(
-            pairFor(factoryAddress,token0, token1)
+        (uint256 reserve0, uint256 reserve1) = IPair(
+            pairFor(factoryAddress, token0, token1)
         ).getReserves();
-        (reserveA,reserveB) = tokenA == token0
-            ? (reserve0, reserve1) : (reserve1,reserve0);
+        (reserveA, reserveB) = tokenA == token0
+            ? (reserve0, reserve1)
+            : (reserve1, reserve0);
     }
 
     function quote(
         uint256 amountIn,
         uint256 reserveIn,
         uint256 reserveOut
-    ) public pure returns (uint256 amountOut){
-        if(amountIn == 0) revert InsufficientAmount();
-        if(reserveIn == 0 || reserveOut == 0) revert InsufficientLiquidity();
-        
+    ) public pure returns (uint256 amountOut) {
+        if (amountIn == 0) revert InsufficientAmount();
+        if (reserveIn == 0 || reserveOut == 0) revert InsufficientLiquidity();
+
         return (amountIn * reserveOut) / reserveIn; //atla e atlu to atla e ketlu evu
     }
 
-    function sortTokens(address tokenA, address tokenB)
-        internal
-        pure
-        returns (address token0,address token1)
-        {
-            return tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        }
-    
+    function sortTokens(
+        address tokenA,
+        address tokenB
+    ) internal pure returns (address token0, address token1) {
+        return tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+    }
+
     function pairFor(
         address factoryAddress,
         address tokenA,
