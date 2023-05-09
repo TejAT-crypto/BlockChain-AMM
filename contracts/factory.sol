81377c04087cb26e4d84879acd103b81a3b50f38
neilaybhatt27 <neilaybhatt27@gmail.com>
2023-05-09 13:59:02 +0530

    Added fees part in factory

--- contracts/factory.sol
+++ contracts/factory.sol
@@ -2,24 +2,29 @@
 pragma solidity ^0.8.0;
 
 import "./DExPair.sol";
-import "../interfaces/IPair.sol";
+import "../interfaces/IFactory.sol";
 
-contract Factory {
+contract Factory is IFactory{
     error SameAddresses();
     error PairExist();
     error ZeroAddress();
+    error IsForbidden(address feesTo, address feesToSetter);
 
-    event CreatedPair(
-        address indexed token_A,
-        address indexed token_B,
-        address pair,
-        uint256
-    );
+    address public feesTo;
+    address public feesToSetter;
 
     mapping(address => mapping(address => address)) public newPairs;
     address[] public allPair;
 
-    function createPairs(address tokenA, address tokenB) public returns (address pair){
+    constructor(address feeToSetter){
+        feesToSetter = feeToSetter;
+    }
+
+    function allPairLength() external view returns (unint256){
+        return allPair.length;
+    }
+
+    function createPairs(address tokenA, address tokenB) external returns (address pair){
         if(tokenA == tokenB){
             revert SameAddresses();
         }
@@ -39,11 +44,14 @@ contract Factory {
         if(newPairs[token_A][token_B] != address(0)){
             revert PairExist();
         }
-        bytes memory ByteCode = type(DExPair).creationCode;
+        //bytes memory ByteCode = type(DExPair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token_A, token_B));
-        assembly{
-            pair := create2(0, add(ByteCode, 32), mload(ByteCode), salt)
-        }
+        // assembly{
+        //     pair := create2(0, add(ByteCode, 32), mload(ByteCode), salt)
+        // }
+
+        pair = address(new DExPair{salt: salt}());
+
         IPair(pair).init(token_A, token_B);
 
         newPairs[token_A][token_B] = pair;
@@ -51,5 +59,19 @@ contract Factory {
         allPair.push(pair);
 
         emit CreatedPair(token_A, token_B, pair, allPair.length);
+    }
+
+    function _setFeesTo(address _feesTo) external {
+        if(msg.sender != feesToSetter){
+            revert IsForbidden(msg.sender, feesToSetter);
+        }
+        feesTo = _feesTo;
+    }
+
+    function _setFeesToSetter(address _feesToSetter) external {
+        if(msg.sender != feesToSetter){
+            revert IsForbidden(msg.sender, feesToSetter);
+        }
+        feesToSetter = _feesToSetter;
     }
 }
--- interfaces/IFactory.sol
+++ interfaces/IFactory.sol
@@ -2,6 +2,23 @@
 pragma solidity ^0.8.10;
 
 interface IFactory {
-    function allPairs(address, address) external pure returns (address);
-    function formPair(address, address) external returns (address); 
+    event CreatedPair(
+        address indexed token_A,
+        address indexed token_B,
+        address pair,
+        uint256
+    );
+
+    function feesTo() external view returns (address);
+    function feesToSetter() external view returns(address);
+
+    function getPairs (address tokenA, address tokenB) external view returns(address pair); 
+
+    function allPair(uint256) external view returns (address pair);
+    function createPairs(address tokenA, address tokenB) external returns (address pair);
+    function allPairLength() external view returns (uint256);
+
+    function _setFeesTo(address) external;
+
+    function _setFeesToSetter(address) external;
 }
