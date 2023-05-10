1427a500575c6921c7a245dc606199d2387d79b6
Samarth Chauhan <samarth.chauhan13022002@gmail.com>
2023-05-10 13:02:00 +0530

    typescript

---
+++ hardhat.config.ts
@@ -0,0 +1,11 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+};
+
+export default config;
